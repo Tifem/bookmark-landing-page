76afd1a81fa49d0281bf6980a9fd2e8f275671a6
Emmanuel Boluwatife Janet <88049758+Tifem@users.noreply.github.com>
2025-09-19 02:48:04 -0700

    fix the features tab section

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+const tabs = document.querySelectorAll("ul li");
+const divs = document.querySelectorAll(".content > div");
+
+tabs.forEach((tab) => {
+
+    tab.addEventListener("click", function(e) {
+        tabs.forEach((tab) => {
+            tab.classList.remove("active")
+        });
+        e.currentTarget.classList.add("active");
+        divs.forEach((div) =>{
+            div.style.display ='none';
+            document.querySelector('.' + e.currentTarget.dataset.content).style.display = 'flex'
+        });
+    })
+})
+
+
+
 const faqs = document.querySelectorAll(".faq-box");
 
 faqs.forEach(faq => {
      faq.addEventListener("click", () => {
          faq.classList.toggle("active");
-     })
+     });
 })
